4965a0a29001b7754acfbb57d26f0dbd554d732e
Shaunak Warty <Shaunak.Warty22@bcp.org>
2018-12-03 15:01:25 -0800

    Added unfinished settings page, changed while loops to while True:, removed Pillow from RacecarDodge

---
+++ setup.py
@@ -0,0 +1,14 @@
+from cx_Freeze import setup, Executable
+from os import environ
+
+environ['TCL_LIBRARY'] = r'C:\Users\Shaunak.Warty22\AppData\Local\Programs\Python\Python37-32\tcl\tcl8.6'
+environ['TK_LIBRARY'] = r'C:\Users\Shaunak.Warty22\AppData\Local\Programs\Python\Python37-32\tcl\tk8.6'
+
+options = {'packages':['pygame', 'dbm'], 'include_files':['Fonts', 'Images']}
+
+setup(
+    name = 'Racecar Dodge',
+    description = 'Use your racecar to dodge obstacles and try to get as many blocks dodged as possible!',
+    options = {'build_exe': options},
+    executables = [Executable('RacecarDodge.py', icon = 'carIcon.ico')]
+)
