9f0426c2460d9785c69d33186193c4cad0fc8e29
Shaunak Warty <37036096+hyperXcube@users.noreply.github.com>
2019-11-12 17:13:38 -0800

    Update gitignore and setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from os import environ
 environ['TCL_LIBRARY'] = r'C:\Users\Shaunak.Warty22\AppData\Local\Programs\Python\Python37-32\tcl\tcl8.6'
 environ['TK_LIBRARY'] = r'C:\Users\Shaunak.Warty22\AppData\Local\Programs\Python\Python37-32\tcl\tk8.6'
 
-options = {'packages':['pygame', 'dbm'],
-           'include_files':['Fonts', 'Images', 'constants.py', 'crash.py', 'functions.py', 'main.py', 'settings.py']}
+options = {'packages': ['pygame', 'dbm'],
+           'include_files': ['Fonts', 'Images', 'constants.py', 'crash.py', 'functions.py', 'main.py', 'settings.py']}
 
 setup(
-    name = 'Racecar Dodge',
-    description = 'Use your racecar to dodge as many obstacles as possible!',
-    options = {'build_exe': options},
-    executables = [Executable('start.py', icon = 'carIcon.ico', targetName = 'Racecar Dodge.exe')]
+    name='Racecar Dodge',
+    description='Use your racecar to dodge as many obstacles as possible!',
+    options={'build_exe': options},
+    executables=[Executable('start.py', icon='carIcon.ico', targetName='Racecar Dodge.exe')]
 )
